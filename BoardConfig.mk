a542154953b9c8db1a166c3be122204baaf7949f
rdndds <rdndds@gmail.com>
2026-04-09 05:24:21 +0700

    fix lun error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,7 @@ TARGET_USES_MKE2FS := true
 
 TW_FRAMERATE := 90
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.usb0/lun.0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file"
 TW_MAX_BRIGHTNESS := 4095
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_NO_SCREEN_BLANK := true
