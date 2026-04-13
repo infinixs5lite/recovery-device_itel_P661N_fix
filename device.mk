6eb5be9be5027eb919af42227cfdc5803ba69a90
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-04-13 23:39:59 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,8 +7,16 @@
 
 LOCAL_PATH := device/itel/P661N
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Enable Virtual A/B OTA
-ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
