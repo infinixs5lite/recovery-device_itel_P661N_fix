5050b97af5774716f7efca4346b39564cae0e8ab
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-04-13 23:42:28 +0100

    Update and rename twrp_P661N.mk to pb_P661N.mk

--- pb_P661N.mk
+++ pb_P661N.mk
@@ -5,24 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Inherit from P661N device
 $(call inherit-product, device/itel/P661N/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := P661N
-PRODUCT_NAME := twrp_P661N
+PRODUCT_NAME := pb_P661N
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel P55 5G
 PRODUCT_MANUFACTURER := ITEL
