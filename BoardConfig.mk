cd0bba8be224afeb57c48fc2c5faae3a12c4ea89
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-05-26 19:41:10 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,6 @@ BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 
 # Vendor modules
 TW_LOAD_VENDOR_BOOT_MODULES := true
-TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_LOAD_VENDOR_MODULES := "flashlight.ko flashlights-aw36515.ko"
 
 # Version
