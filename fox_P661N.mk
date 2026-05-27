e08e0c7fa3773c79d8abbbeb8f4e5a6f08ad72a9
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-05-27 10:39:08 +0100

    Update fox_P661N.mk

--- fox_P661N.mk
+++ fox_P661N.mk
@@ -23,6 +23,7 @@
 # flashlight
 OF_FLASHLIGHT_ENABLE := 1
 OF_FL_PATH1="/sys/class/torch/torch/torch_level"
-	
+ OF_FL_PATH2="/system/etc/flashlight"
+
 # legacy services for battery
 OF_USE_LEGACY_BATTERY_SERVICES := 1
