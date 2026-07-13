a095cd32b5178f922048dca744d5d069cb68d5ae
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-13 22:06:15 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from mt6833-common
-$(call inherit-product, device/transsion/mt6833-common/common.mk)
+# Inherit from common-mt6833
+$(call inherit-product, transsion/device/common-mt6833/common.mk)
