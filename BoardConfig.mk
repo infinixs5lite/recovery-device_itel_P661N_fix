72db37a716eb9ec4e5cfa453d1cba9617d05131f
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-13 21:30:23 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,10 @@ include device/transsion/mt6833-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := P661N
 
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_itel-P661N
+TARGET_RECOVERY_DEVICE_MODULES := libinit_itel-P661N
+
 # Version
 TW_DEVICE_VERSION := NINO
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
